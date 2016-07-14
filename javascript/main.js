cdae93129a4460b3f271465a473b9e71728e384d
Olina Stathopoulou <olina.stath@nyu.edu>
2016-07-14 21:30:37 +0300

    finished layout

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,26 @@
+// $("a[href*='" + location.pathname + "']").css("color", "#e6c52c");
 $(document).ready(function() {
 	$('.landing').animate({opacity: '1'}, 2500, function() {
 		$('#enter').animate({opacity: '0.8'}, 1500);
 	});
+
+	$('body').animate({opacity: '1'}, 2500);
+
+	slideshow(1);
+
+});
+
+var count = 1;
+$(function slideshow(count) {
+	if (count <= 4) {
+		$('span.slide#img' + count).fadeIn(4000, function() {
+			$('span.slide#img' + count).fadeOut(4000);
+			count++;
+			slideshow(count, 'span.slide#img' + count);
+		});
+		
+	} else {
+		count = 1;
+		slideshow(count, 'span.slide#img' + count);
+	}
 });
